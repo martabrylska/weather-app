d4040ccf455f41b8a9b9c732b2f284aafc69dee1
Marta Brylska <tomica.marta@gmail.com>
2024-01-12 12:41:23 +0100

    repaired case when '-0' value appeared.

--- src/components/NextDaysParams/SingleDayWeather.tsx
+++ src/components/NextDaysParams/SingleDayWeather.tsx
@@ -26,13 +26,13 @@ export const SingleDayWeather = (props: Props) => {
                     alt={`${props.nextDaysWeather.desc}`}
                     src={`https://openweathermap.org/img/wn/${props.nextDaysWeather.icon}@2x.png`}></img>
                 <p>
-                    {`${Number(props.nextDaysWeather.temp).toFixed()}°`}
+                    {`${Number(props.nextDaysWeather.temp).toFixed() === '-0' ? '0' : Number(props.nextDaysWeather.temp).toFixed()}°`}
                 </p>
                 <img
                     alt={`${props.nextNightsWeather.desc}`}
                     src={`https://openweathermap.org/img/wn/${props.nextNightsWeather.icon}@2x.png`}></img>
                 <p>
-                    {`${Number(props.nextNightsWeather.temp).toFixed()}°`}
+                    {`${Number(props.nextNightsWeather.temp).toFixed() === '-0' ? '0' : Number(props.nextNightsWeather.temp).toFixed()}°`}
                 </p>
             </div>
             <div className="line"></div>
